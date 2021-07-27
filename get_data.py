b03246fd77f457143b374fc57c7b91ef09e87d0b
dominik.lenda <dlenda1729@gmail.com>
2021-07-27 14:34:57 +0200

    ingest data and get mean earthquake magnitude

--- get_data.py
+++ get_data.py
@@ -1,6 +1,18 @@
-# https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
-
 import requests
 
-resp = requests.get('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_hour.geojson')
-print(resp.json())
+url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson'
+
+def get_magnitudes(url):
+  url = url
+  resp = requests.get(url)
+  data = resp.json()
+  earthquakes_number = len(data['features'])
+  magnitudes = [data['features'][i]['properties']['mag'] for i in range(earthquakes_number-1)]
+  return magnitudes
+
+def averageOfList(numOfList):
+  avg = sum(numOfList) / len(numOfList)
+  return avg
+
+magnitudes = get_magnitudes(url)
+print(averageOfList(magnitudes))
