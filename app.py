b802f634f7dce7455511180abf796b793372174a
dominik.lenda <dlenda1729@gmail.com>
2021-08-06 15:25:15 +0200

    test deployment

--- app.py
+++ app.py
@@ -2,7 +2,22 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objects as go
-from extract_data import *
+# from extract_data import *
+
+import pandas as pd
+import requests
+
+def extract_coordinates(url):
+  """Extracts coordinates of earthquakes."""
+  resp = requests.get(url)
+  data = resp.json()
+  earthquakes_number = len(data['features'])
+  coordinates = [data['features'][i]['geometry']['coordinates'] for i in range(earthquakes_number)]
+  longitude = [cord[0] for cord in coordinates]
+  latitude = [cord[1] for cord in coordinates]
+  d = {'longitude': longitude, 'latitude': latitude}
+  df = pd.DataFrame(data=d)
+  return df
 
 app = dash.Dash(__name__)
 url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson'
